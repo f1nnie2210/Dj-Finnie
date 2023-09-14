1d21d31afd076e46cb47544e997a864b4e165dc0
f1nnie2210 <longnguyen03072001@gmail.com>
2023-09-14 21:06:53 +0700

    prettier for all

--- commands/code.js
+++ commands/code.js
@@ -1,16 +1,22 @@
 const { SlashCommandBuilder, messageLink } = require('discord.js');
 module.exports = {
-  name: "code",
-  description: "Stardew Valley invite code.", 
-  permissions: "0x0000000000000800",
-  options: [],
-  run: async (client, interaction) => {
-	const codeCommand = new SlashCommandBuilder()
-  var fs = require('fs');
-  fs.readFile('C://Program Files (x86)/Steam/steamapps/common/Stardew Valley/Mods/Always On Server/InviteCode.txt', 'utf8', function(err, data) {
-    if (err) throw err;
-      console.log(data);
-      interaction.reply("Enter this code to join the Stardew Valley server: " + data);
-    });
-  }
-}
+    name: 'code',
+    description: 'Stardew Valley invite code.',
+    permissions: '0x0000000000000800',
+    options: [],
+    run: async (client, interaction) => {
+        const codeCommand = new SlashCommandBuilder();
+        var fs = require('fs');
+        fs.readFile(
+            'C://Program Files (x86)/Steam/steamapps/common/Stardew Valley/Mods/Always On Server/InviteCode.txt',
+            'utf8',
+            function (err, data) {
+                if (err) throw err;
+                console.log(data);
+                interaction.reply(
+                    'Enter this code to join the Stardew Valley server: ' + data
+                );
+            }
+        );
+    }
+};
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,18 +1,17 @@
-const db = require("../mongoDB");
+const db = require('../mongoDB');
 
 module.exports = async (client, message) => {
-  if (message.author.bot || !message.guild) return;
-  const data = 
-    (await db.blacklists.findOne({ guildID: message.guild.id})) ||
-    (await db.blacklists.create({ guildID: message.guild.id }));
+    if (message.author.bot || !message.guild) return;
+    const data =
+        (await db.blacklists.findOne({ guildID: message.guild.id })) ||
+        (await db.blacklists.create({ guildID: message.guild.id }));
 
     const bannedWords = data.words.map((x) => x.toLowerCase().trim());
     const messageWords = message.content.toLowerCase().split(/\s+/);
-    
+
     if (messageWords.some((word) => bannedWords.includes(word))) {
-      message.delete();
-      const user = message.author;
-      message.channel.send({ content: `Vô văn hoá! ${user}` });
+        message.delete();
+        const user = message.author;
+        message.channel.send({ content: `Vô văn hoá! ${user}` });
     }
-    
-}
+};
